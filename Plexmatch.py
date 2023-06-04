8d26af2f77b91721d68b177e73f19f1e5e14d8e9
fzlins <100051558+fzlins@users.noreply.github.com>
2023-06-04 16:55:58 -0400

    Update Plexmatch.py

--- Plexmatch.py
+++ Plexmatch.py
@@ -11,13 +11,8 @@ parser.add_argument("-o", "--order", action='store_true', help="Select only orde
 parser.add_argument("-ow", "--overwrite", action='store_true', help="Overwirte .plexmatch file.")
 parser.add_argument("-s", "--season", help="Specify a season number.")
 args = parser.parse_args()
+
 directory = args.directory
-
-video_exts = ['3g2', '3gp', 'asf', 'asx', 'avc', 'avi', 'avs', 'bivx', 'bup', 'divx', 'dv', 'dvr-ms', 'evo', 'fli', 'flv',
-              'm2t', 'm2ts', 'm2v', 'm4v', 'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'mts', 'nsv', 'nuv', 'ogm', 'ogv', 'tp',
-              'pva', 'qt', 'rm', 'rmvb', 'sdp', 'svq3', 'strm', 'ts', 'ty', 'vdr', 'viv', 'vob', 'vp3', 'wmv', 'wtv', 'xsp', 'xvid', 'webm']
-
-
 plexmatchFilename = '.plexmatch'
 plexmatchFilePath = os.path.join(directory, plexmatchFilename)
 
@@ -26,7 +21,22 @@ if os.path.exists(plexmatchFilePath):
         os.remove(plexmatchFilePath)
     else:
         quit()
-    
+
+from natsort import os_sorted
+filelist = os_sorted(os.listdir(directory))
+video_exts = ['3g2', '3gp', 'asf', 'asx', 'avc', 'avi', 'avs', 'bivx', 'bup', 'divx', 'dv', 'dvr-ms', 'evo', 'fli', 'flv',
+              'm2t', 'm2ts', 'm2v', 'm4v', 'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'mts', 'nsv', 'nuv', 'ogm', 'ogv', 'tp',
+              'pva', 'qt', 'rm', 'rmvb', 'sdp', 'svq3', 'strm', 'ts', 'ty', 'vdr', 'viv', 'vob', 'vp3', 'wmv', 'wtv', 'xsp', 'xvid', 'webm']
+
+skip_files = [plexmatchFilename, '[SP]', '[MENU]']
+filelist = list(filter(lambda x: not any(map(x.__contains__, skip_files)) and os.path.isfile(os.path.join(directory, x)) and x.rsplit('.', 1)[-1] in video_exts, filelist))
+
+commonprefix = os.path.commonprefix(filelist)
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
 with open(plexmatchFilePath,"a+", encoding="utf-8-sig") as f:
     print("# Regular episodes with tricky filenames", file=f)
     season_number = '01'
@@ -34,19 +44,12 @@ with open(plexmatchFilePath,"a+", encoding="utf-8-sig") as f:
         season_number = args.season
 
     index = 1
-    for filename in os.listdir(directory):
-        if filename == plexmatchFilename:
+    for filename in filelist:
+        ep = re.search(r'\d{2,3}', remove_prefix(filename, commonprefix)).group()
+        if (args.order and index != int(ep)):
             continue
-        
-        filepath = os.path.join(directory, filename)
-        if os.path.isfile(filepath):
-            file_extension = filename.rsplit('.', 1)
-            if file_extension[-1] in video_exts:
-                ep = re.search(r'\d{2,3}', filename).group()
-                if (args.order and index != int(ep)):
-                    continue
 
-                index = index + 1
-                line = f"ep: S{season_number}E{ep}: {filename}"
-                #print(line)
-                print(line, file=f)
+        index = index + 1
+        line = f"ep: S{season_number}E{ep}: {filename}"
+        #print(line)
+        print(line, file=f)
